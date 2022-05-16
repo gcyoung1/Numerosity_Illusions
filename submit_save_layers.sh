57185f5dd3502eb148b8073c7668abd9691eeaf6
Griffin Crosby Young <gcyoung@stanford.edu>
2022-05-15 23:21:30 -0700

    modifying save_layers run for test

--- submit_save_layers.sh
+++ submit_save_layers.sh
@@ -11,6 +11,7 @@
 
 source activate numerosity_illusions
 
-for stim_dir in `ls data/stimuli|head -1`;do echo $stim_dir; python -m scripts.models.save_layers --model alexnet --pretrained --stimulus_directory $stim_dir --layers features_0 features_1 features_2 features_3 features_4 features_5 features_6 features_7 features_8 features_9 features_10 features_11 features_12 classifier_1 classifier_2 classifier_4 classifier_5 classifier_6 --num_workers 1;done
+#features_0 features_1 features_2 features_3 features_4 features_5 features_6 features_7 features_8 features_9 features_10 features_11
+for stim_dir in `ls data/stimuli|head -1`;do echo $stim_dir; python -m scripts.models.save_layers --model alexnet --pretrained --stimulus_directory $stim_dir --layers features_12 classifier_1 classifier_2 classifier_4 classifier_5 classifier_6 --num_workers 1;done
 #python save_layers.py --model alexnet --pretrained --stimulus_directory $stim_dir --layers features_0 features_1 features_2 features_3 features_4 features_5 features_6 features_7 features_8 features_9 features_10 features_11 features_12 classifier_1 classifier_2 classifier_4 classifier_5 classifier_6 --num_workers 1;done
 
