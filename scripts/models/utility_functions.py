0d3e7c764e3238c06bc65a5a1486b8f9cc96f9a4
Griffin Crosby Young <gcyoung@stanford.edu>
2022-11-27 00:19:26 -0800

    layers now have __ between them, loading new models, now returning data transform as well

--- scripts/models/utility_functions.py
+++ scripts/models/utility_functions.py
@@ -1,7 +1,14 @@
 import os
 from torchvision import models
+from torchvision import transforms
 from torchinfo import summary
 from functools import reduce
+from pytorch_pretrained_vit import ViT
+from .resmlp import resmlp_24
+from .deit import deit_tiny_patch16_224
+from .alexnet_fractaldb import alexnet_fractaldb
+from .densenet import DenseNet
+from .identitynet import IdentityNet
 
 
 def tensorToNumpy(tensor):
@@ -18,8 +25,7 @@ def writeAndFlush(csv_file, line:str):
     csv_file.write(line + '\n')
     csv_file.flush()
 
-def createActivationCSV(folder,features_size:int):
-    column_list = ['numerosity','size','spacing','num_lines']
+def createActivationCSV(folder, column_list:list, features_size:int):
     parameters_header = listToString(column_list)
     neuron_names = [f'n{i}' for i in range(features_size)]
     activations_header = listToString(neuron_names)
@@ -29,24 +35,115 @@ def createActivationCSV(folder,features_size:int):
     writeAndFlush(csv_file, header)
     return csv_file
 
-def initializeModel(model_name:str, pretrained:bool):
-    if model_name == "alexnet":
-        model = models.alexnet(pretrained=pretrained)
+def initializeModel(model_name:str):
+    if model_name == "alexnet_pretrained":
+        model = models.alexnet(pretrained=True)
         input_size = 224
+        data_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
 
-    elif model_name == "cornet_s":
-        raise NotImplementedError("I haven't added the CORnet code yet.")
-        model = cornet.cornet_s(pretrained=pretrained)
+    elif model_name == "alexnet_fractaldb":
+        model = alexnet_fractaldb()
         input_size = 224
+        data_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+        ])
+
+
+    elif model_name == "alexnet_random":
+        model = models.alexnet(pretrained=False)
+        input_size = 224
+        data_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
+    
+    elif model_name == "densenet":
+        model = DenseNet()
+        input_size = 224
+        data_transform = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+
+
+    elif model_name == "identitynet":
+        model = IdentityNet()
+        input_size = 224
+        data_transform = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+
+    elif model_name == "vit_pretrained":
+        model = ViT('B_16_imagenet1k', pretrained=True)
+        input_size = 384
+        data_transform = transforms.Compose([
+            transforms.Resize((384, 384)), 
+            transforms.ToTensor(),
+            transforms.Normalize(0.5, 0.5),
+        ])
+
+
+    elif model_name == "vit_random":
+        model = ViT('B_16_imagenet1k', pretrained=False)
+        input_size = 384
+        data_transform = transforms.Compose([
+            transforms.Resize((384, 384)), 
+            transforms.ToTensor(),
+            transforms.Normalize(0.5, 0.5),
+        ])
+
+    elif model_name == "deit_pretrained":
+        model = deit_tiny_patch16_224(pretrained=True, dataset='imagenet')
+        input_size = 224
+        data_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
+    elif model_name == "deit_fractaldb":
+        model = deit_tiny_patch16_224(pretrained=True, dataset='fractaldb')
+        input_size = 224
+        data_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+        ])
+
+    elif model_name == "deit_random":
+        model = deit_tiny_patch16_224(pretrained=False)
+        input_size = 224
+        data_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
+    elif model_name == "resmlp24_pretrained":
+        model = resmlp_24(pretrained=True)
+        input_size = 224
+        data_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
+    elif model_name == "resmlp24_random":
+        model = resmlp_24(pretrained=False)
+        input_size = 224
+        data_transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
 
     else:
         raise ValueError("Model name not recognized")
 
     noGrad(model)
-    return model, input_size
+    return model, data_transform, input_size
     
 def get_layer_size_dict(model_name:str):
-    model, input_size = initializeModel(model_name, False)
+    model, data_transform, input_size = initializeModel(model_name)
     model_stats = summary(model, input_size = (1,3,input_size, input_size), col_names=["output_size"])
 
     layer_size_dict = {}
@@ -77,7 +174,7 @@ def populate_layer_size_dict(name, layer_info, layer_size_dict):
         if len(name) == 0:
             child_name = child.var_name
         else:
-            child_name = f"{name}_{child.var_name}"
+            child_name = f"{name}__{child.var_name}"
         populate_layer_size_dict(child_name, child, layer_size_dict)
         # LayerInfo objects' children attribute contains all descendents, 
         # so we skip the ones which were already added to the layer_info_dict
