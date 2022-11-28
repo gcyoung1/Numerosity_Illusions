533d37957d2b7c87e4808e7cebc15382b470fe71
Griffin Crosby Young <gcyoung@stanford.edu>
2022-11-27 17:12:20 -0800

    Bugs in get_layer_size_dict

--- scripts/models/utility_functions.py
+++ scripts/models/utility_functions.py
@@ -173,11 +173,11 @@ def populate_sizehook_dict(surname, module, sizehook_dict):
     if len(submodules) == 0:
         # Base case: if you have no submodules, create a hook and add yourself to the dict
         hook = SizeHook(module)
-        sizehook_dict[name] = hook
+        sizehook_dict[surname] = hook
         return None
 
     for submodule_name, submodule in submodules.items():
         # Recursive case: if you have submodules, pass down your name
-        surname = f"{name}_{submodule_name}" if name != "" else submodule_name
-        populate_sizehook_dict(surname, submodule, sizehook_dict)
+        new_surname = f"{surname}_{submodule_name}" if surname != "" else submodule_name
+        populate_sizehook_dict(new_surname, submodule, sizehook_dict)
     return None
